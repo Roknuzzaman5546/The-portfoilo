59ce20b8e64f4856211241f57765188e3ed7b001
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-04-05 17:01:01 +0600

    skills services code added

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
                         <h2 className=" text-5xl font-bold text-white my-4">John doe</h2>
                         <p className=" text-[#d5d5d5] leading-loose">As a React developer with 5 years of experience, I have honed my skills in JavaScript and am currently learning TypeScript to expand my expertise. I pride myself on being a quick learner and attentive listener, which allows me to collaborate effectively with clients to create efficient and scalable solutions. My focus is on developing user-friendly applications that solve real-world problems.",
                             "quote": "Live the Life as you want</p>
-                        <button className="button-primary hover:bg-[#ff714a] mt-3 text-white font-serif hover:text-black">
+                        <button className="button-orange hover:bg-[#ff714a] mt-3 text-white font-serif hover:text-black text-xl">
                             contact
                         </button>
                     </div>
@@ -36,7 +36,15 @@ const About = () => {
             </div>
             {/* client and what i do section */}
             <div className=' p-[40px] md:p-[80px]'>
-
+                <h1 className=' text-2xl font-bold inline-block border-b-[4px] border-[#ff714a] px-2'>Clients</h1>
+                <div className='flex justify-center items-around mt-2'>
+                    <img className=' h-9' src="https://i.ibb.co/bJ8W313/client-1.png" alt="" />
+                    <img className=' h-9' src="https://i.ibb.co/7v9WVhj/client-2.png" alt="" />
+                    <img className=' h-9' src="https://i.ibb.co/zNYrr2w/client-3.png" alt="" />
+                    <img className=' h-9' src="https://i.ibb.co/0qs8JTL/client-4.png" alt="" />
+                    <img className=' h-9' src="https://i.ibb.co/HpgJh15/client-5.png" alt="" />
+                    <img className=' h-9' src="https://i.ibb.co/Sv5GwVf/client-6.png" alt="" />
+                </div>
             </div>
         </div>
     );
--- src/Components/Pages/Skills/Skills.jsx
+++ src/Components/Pages/Skills/Skills.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
 
 const Skills = () => {
+    const [service, setService] = useState()
+    // console.log(service);
+
+    useEffect(() => {
+        fetch('/portfolio.json')
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [])
+
+    const { user } = service || {};
+    const { skills } = user || {};
+    // console.log(skills);
     return (
-        <div>
-            <h3>This is skills page</h3>            
+        <div
+            data-aos="zoom-in-up"
+            data-aos-anchor-placement="top-center"
+            className=' p-5 md:p-10'>
+            {/* services header section */}
+            <h2 className='text-3xl font-bold border-b-[4px] border-[#ff714a] px-2 pb-2 text-white'>My Skills</h2>
+            <h4 className=" text-right text-[#aaaa] text-xl mt-1">My skills</h4>
+            {/* my skills bode part */}
+            <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
+                {
+                    skills.map(item => (
+                        <div key={item._id}>
+                            <img className=" h-12" src={item?.image?.url} alt="" />
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
